e4881bcb801e414261b6c45d648a85b09f3a97a5
AvramAlex8 <alexandru-c.avram@student.upt.ro>
2021-05-16 22:34:15 +0300

    Search course provider

---
+++ src/main/java/controllers/SearchCourseProviderController.java
@@ -0,0 +1,72 @@
+package controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.exceptions.CourseNameIncorrectException;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SearchCourseProviderController implements Initializable {
+
+    @FXML
+    public Label closeButton;
+    public Label homePageButton;
+    public Label emptyFieldMessage;
+    public TextField searchCourseName;
+    public Button searchButton;
+    public Label courseNameIncorrect;
+
+    @FXML
+    public void handleSearchButton(MouseEvent event) throws IOException {
+        try{
+            if(Provider.searchCourseProvider(searchCourseName.getText()))
+            {
+                Parent informationPage = FXMLLoader.load(getClass().getResource("InformationCourseProvider.fxml"));
+                Scene informationScene = new Scene(informationPage);
+                Stage appStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                appStage.setScene(informationScene);
+                appStage.show();
+            }
+            else
+                throw new CourseNameIncorrectException();
+        } catch(CourseNameIncorrectException e)
+        {
+            courseNameIncorrect.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void handleHomePageButton(MouseEvent event) throws IOException
+    {
+
+        Parent HomePage = FXMLLoader.load(getClass().getResource("HomePageProvider.fxml"));
+        Scene HomeScene = new Scene(HomePage);
+        Stage appStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        appStage.setScene(HomeScene);
+        appStage.show();
+    }
+
+    @FXML
+    public void handleCloseButtonAction(MouseEvent event)
+    {
+        if(event.getSource() == closeButton)
+        {
+            System.exit(0);
+        }
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
